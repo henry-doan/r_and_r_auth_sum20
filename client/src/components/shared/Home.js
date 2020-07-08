9f202f761aeaaa931732e21b9e402bfa84590854
Henry Doan <henrydoan96@gmail.com>
2020-07-07 18:49:01 -0600

    styled

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { HomeHeader, CoolImg, HomeContainer, Btn, Star } from '../styledComp/HomeStyles';
+import P from '../../images/pineapple.jpeg';
+import { Icon } from 'semantic-ui-react';
 
 const Home = () => (
-  <h1>Home page</h1>
+  <HomeContainer>
+    <h1 style={{ background: 'blue' }}>Home page</h1>
+    <h1 style={styles.headerStyles}>Home page</h1>
+    <HomeHeader fSize='lrg'>Hello</HomeHeader>
+    <HomeHeader fSize='sm'>Hello</HomeHeader>
+    <HomeHeader>Hello</HomeHeader>
+    <CoolImg src={P} />
+    <Btn>Click</Btn>
+    <br />
+    <Star>
+      <Icon name='star' />
+    </Star>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </HomeContainer>
 )
+
+
+export const styles = {
+  headerStyles: { 
+    background: 'blue',
+    color: 'white',
+    fontSize: '18px',
+  },
+  paragraphStyles: {
+    color: 'green'
+  }
+}
 
 export default Home;
